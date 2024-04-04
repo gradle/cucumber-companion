0cf6f92e686a972c56982de2c97489f5ca63887b
Pavlo Shevchenko <pshevchenko@gradle.com>
2024-04-04 11:55:01 +0200

    Replace GBT convention plugin w/ CCUD

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,9 +10,8 @@ pluginManagement {
 }
 
 plugins {
-    id("com.gradle.enterprise") version ("3.14.1")
-    id("com.gradle.enterprise.gradle-enterprise-conventions-plugin") version ("0.7.4")
-    id("com.gradle.common-custom-user-data-gradle-plugin") version ("1.11.1")
+    id("com.gradle.develocity") version ("3.17")
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "1.13"
 }
 
 dependencyResolutionManagement {
@@ -29,3 +28,9 @@ rootProject.name = "cucumber-companion"
 include("gradle-plugin")
 include("maven-plugin")
 include("companion-generator")
+
+develocity {
+    buildScan {
+        publishing.onlyIf { false }
+    }
+}
