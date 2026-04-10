4a98370d6aac093ba846049887cc91ca7480330b
bot-gh-dv-shared <bot-gh-dv-shared+renovate@gradle.com>
2026-04-10 07:14:29 +0200

    [Renovate Bot] Update plugin com.gradle.common-custom-user-data-gradle-plugin to v2.6.0 (#259)
    Signed-off-by: Renovate Bot for Gradle <bot-gh-dv-shared+renovate@gradle.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,7 @@ pluginManagement {
 
 plugins {
     id("com.gradle.develocity").version("4.4.0")
-    id("com.gradle.common-custom-user-data-gradle-plugin") version "2.5.0"
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "2.6.0"
 }
 
 dependencyResolutionManagement {
